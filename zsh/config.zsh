f82052c43b0a8f765374967f48e7f261d40e7456
Chris Hausler <chausler@zendesk.com>
2014-12-03 15:53:42 +1100

    bug fic

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -7,10 +7,6 @@ fi
 export LSCOLORS="ExGxFxDxCxDxDxhbhdacEc"
 export CLICOLOR=true
 
-fpath=($ZSH/functions $fpath)
-
-autoload -U $ZSH/functions/*(:t)
-
 HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
