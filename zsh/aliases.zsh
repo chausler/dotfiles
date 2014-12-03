6dc240e82ce330a067376c5b7fb1da3924546b5c
Chris Hausler <chausler@zendesk.com>
2014-12-03 15:45:06 +1100

    update my dotfiles

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,8 @@
 alias reload!='. ~/.zshrc'
 alias c='clear'
 alias be='bundle exec'
-alias ls="ls -l --color=always"
+alias ls="ls -l"
 
 alias apps='cd ~/code/zendesk/apps'
+alias zendesk='cd ~/code/zendesk'
+alias code='cd ~/code'
