6a639055536feb501c5db084d686950ab6c0aeb7
Chris Hausler <chausler@zendesk.com>
2014-12-03 16:03:35 +1100

    updates

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -35,3 +35,11 @@ setopt complete_aliases
 
 zle -N newtab
 bindkey -v
+
+bindkey '^P' up-history
+bindkey '^N' down-history
+bindkey '^?' backward-delete-char
+bindkey '^h' backward-delete-char
+bindkey '^w' backward-kill-word
+bindkey '^r' history-incremental-search-backward
+export KEYTIMEOUT=1
